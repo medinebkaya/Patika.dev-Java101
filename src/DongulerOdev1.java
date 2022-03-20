526aabec323a7e68c53ce4d719d8e6e2e550a1ad
medinebkaya <77451962+medinebkaya@users.noreply.github.com>
2022-03-20 21:10:54 +0300

    Update and rename GirilenSayiyaKadarOlanCiftSayilariBulanProgram.java to DongulerOdev1.java

--- src/DongulerOdev1.java
+++ src/DongulerOdev1.java
@@ -1,18 +1,23 @@
+//java döngüler ile 0'dan girilen sayıya kadar olan sayılardan 3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan program
 import java.util.Scanner;
 public class Pratik1 {
     public static void main(String[] args) {
         int k;
         int toplam=0;
+        int sayisayisi=0;
         Scanner input=new Scanner(System.in);
         System.out.print("Sayı giriniz: ");
         k=input.nextInt();
 
-        for (int i=1; i<=k; i++)
+        for (int i=0; i<=k; i++)
         {
             if (i%3==0 && i%4==0){
             System.out.println(i);
             toplam+=i;
+            sayisayisi++;
             }
         }
+        double ortalama=toplam/sayisayisi;
+        System.out.println("ortalama: "+ortalama);
     }
 }
